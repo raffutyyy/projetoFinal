c7292407f33963b5947769dbea4fdad847586eda
Raffaela <raffa.fdo@gmail.com>
2024-10-22 21:33:55 -0300

    atualização no case 2

--- script.js
+++ script.js
@@ -1,47 +1,70 @@
-console.log('===== ABRIGOS TEMPORÁRIOS =====')
-console.log('1. Cadastrar abrigo')
-console.log('2. Listar abrigos')
-console.log('3. Procurar abrigo')
-console.log('4. Sair')
-console.log('Escolha uma opção:')
+let alternativa = ''
+function cabecalho() {
+    console.log('===== ABRIGOS TEMPORÁRIOS =====')
+    console.log('1. Cadastrar abrigo')
+    console.log('2. Listar abrigos')
+    console.log('3. Procurar abrigo')
+    console.log('4. Sair')
+    console.log('Escolha uma opção:')
+    alternativa = Number(prompt('Escolha uma opção:'))
+
+   
+    
+
+}
+cabecalho()
 
 
 function SistemaAbrigo() {
-    let alternativa = Number(prompt('Escolha uma opção:'))
     let abrigo = []
-
+    let totalAbrigo = [abrigo]
     switch (alternativa) {
         case 1:
             let nome = prompt('Nome do abrigo:')
             let endereco = prompt('Endereço do abrigo:')
             let telefone = Number(prompt('Telefone para contato:'))
             let capacidade = Number(prompt('Capacidade de lotação:'))
+            let cidade = prompt('Cidade do abrigo:')
 
             let dadosAbrigo = {
                 nome: nome,
                 endereco: endereco,
                 telefone: telefone,
-                capacidade: capacidade
+                capacidade: capacidade,
+                cidade: cidade
 
             }
             abrigo.push(dadosAbrigo)
-            console.log("Abrigo cadastrado com sucesso!") // tirar dps , feito apenas para analisar 
+            console.log("===== Abrigo cadastrado com sucesso! =====")
             console.log(abrigo) // tirar também
-
-
-
             break
         case 2:
+            console.log('--------------------')
+            console.log('LISTAGEM DE ABRIGOS:')
+            console.log('--------------------')
+            if (totalAbrigo >= 1) {
+                for (let listaAbrigo = 0; listaAbrigo > abrigo.length; listaAbrigo++) {
+                    console.log(`  ${abrigo[0]} | ${abrigo[1]}      | ${abrigo[2]}     |  ${abrigo[3]}  |     ${abrigo[4]} | ${abrigo[5]}`)
+
+                }
+            } else{
+                console.log('Ainda nao há abrigos cadastrados! Tente novamente.')
+            }
 
             break
         case 3:
 
+
             break
         case 4:
-
+                console.log('Saindo...')
+              
             break
 
     }
+    cabecalho()
 
 }
+
 SistemaAbrigo()
+
