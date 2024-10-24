7e7d494c8ab1f60632893c7848c86fb8a1d0cf5f
Raffutyyy.Raffaela <Raffa.fdo@gmail.com>
2024-10-24 16:08:20 -0300

    Projeto concluído!

--- script.js
+++ script.js
@@ -1,47 +1,88 @@
-console.log('===== ABRIGOS TEMPORÁRIOS =====')
-console.log('1. Cadastrar abrigo')
-console.log('2. Listar abrigos')
-console.log('3. Procurar abrigo')
-console.log('4. Sair')
-console.log('Escolha uma opção:')
+let alternativa = ''
+let abrigo = []
+function cabecalho() {
+    console.log('===== ABRIGOS TEMPORÁRIOS =====')
+    console.log('1. Cadastrar abrigo')
+    console.log('2. Listar abrigos')
+    console.log('3. Procurar abrigo')
+    console.log('4. Sair')
+    console.log('Escolha uma opção:')
+    alternativa = Number(prompt('Escolha uma opção:'))
+    console.log('-----------------------------------')
+
+}
+
 
 
 function SistemaAbrigo() {
-    let alternativa = Number(prompt('Escolha uma opção:'))
-    let abrigo = []
 
-    switch (alternativa) {
-        case 1:
-            let nome = prompt('Nome do abrigo:')
-            let endereco = prompt('Endereço do abrigo:')
-            let telefone = Number(prompt('Telefone para contato:'))
-            let capacidade = Number(prompt('Capacidade de lotação:'))
+    while (true) {
+        cabecalho()
+        switch (alternativa) {
+            case 1:
+                let nome = prompt('Nome do abrigo:')
+                let endereco = prompt('Endereço do abrigo:')
+                let telefone = Number(prompt('Telefone para contato:'))
+                let capacidade = Number(prompt('Capacidade de lotação:'))
+                let cidade = prompt('Cidade do abrigo:')
 
-            let dadosAbrigo = {
-                nome: nome,
-                endereco: endereco,
-                telefone: telefone,
-                capacidade: capacidade
+                let dadosAbrigo = {
+                    nome: nome,
+                    endereco: endereco,
+                    telefone: telefone,
+                    capacidade: capacidade,
+                    cidade: cidade
 
-            }
-            abrigo.push(dadosAbrigo)
-            console.log("Abrigo cadastrado com sucesso!") // tirar dps , feito apenas para analisar 
-            console.log(abrigo) // tirar também
+                }
+                abrigo.push(dadosAbrigo)
+                console.log("===== Abrigo cadastrado com sucesso! =====")
 
+                break
+            case 2:
+                console.log('--------------------')
+                console.log('LISTAGEM DE ABRIGOS:')
+                console.log('--------------------')
+                console.log(`CÓDIGO | NOME | ENDEREÇO | TELEFONE | CAPACIDADE | CIDADE`)
+                console.log(`-----------------------------------------------------------`)
+                if (abrigo.length > 0) {
+                    for (let i = 0; i < abrigo.length; i++) {
+                        console.log(`  ${[i + 1]}  |   ${abrigo[i].nome}   |   ${abrigo[i].endereco}   |  ${abrigo[i].telefone}   |   ${abrigo[i].capacidade}   |   ${abrigo[i].cidade}  `)
+                        console.log(`----------------------------------------------------------`)
 
+                    }
+                } else {
+                    console.log('Ainda nao há abrigos cadastrados! Tente novamente.')
+                }
 
-            break
-        case 2:
+                break
+            case 3:
+                let solicitaCidade = prompt('Qual cidade você está?')
+                let abrigoEncontrado = abrigo.filter(novoAbrigo => novoAbrigo.cidade === solicitaCidade)
+                console.log('Qual cidade você está?')
+                console.log(solicitaCidade)
+                console.log('--------------------')
+                console.log('LISTAGEM DE ABRIGOS:')
+                console.log('--------------------')
+                console.log(`CÓDIGO | NOME | ENDEREÇO | TELEFONE | CAPACIDADE | CIDADE`)
+                console.log(`-----------------------------------------------------------`)
+                if (abrigoEncontrado.length > 0) {
+                    for (let i = 0; i < abrigoEncontrado.length; i++) {
+                        console.log(`  ${[i + 1]}  |   ${abrigoEncontrado[i].nome}   |   ${abrigoEncontrado[i].endereco}   |  ${abrigoEncontrado[i].telefone}   |   ${abrigoEncontrado[i].capacidade}   |   ${abrigoEncontrado[i].cidade}  `)
+                        console.log(`----------------------------------------------------------`)
 
-            break
-        case 3:
+                    }
+                } else {
+                    console.log('Não foi encontrado nenhum abrigo na sua cidade :(')
+                }
+                break
+            case 4:
+                console.log('Saindo...')
+                return
 
-            break
-        case 4:
-
-            break
+            default:
+                console.log('Opção inválida! Tente novamente.');
+        }
 
     }
-
 }
 SistemaAbrigo()
